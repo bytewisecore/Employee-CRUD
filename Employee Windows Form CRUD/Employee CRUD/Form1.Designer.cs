44dcd4d197f335c8e28a6c0c0fa9c47571a619e6
ahti-mughal <ahti.mughal7866@gmail.com>
2022-10-18 09:19:07 +0500

    Creted About Us section

--- Employee Windows Form CRUD/Employee CRUD/Form1.Designer.cs
+++ Employee Windows Form CRUD/Employee CRUD/Form1.Designer.cs
@@ -56,77 +56,69 @@ namespace Employee_CRUD
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 52);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(12, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.Size = new System.Drawing.Size(57, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "First Name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(213, 52);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(160, 42);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.Size = new System.Drawing.Size(58, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Last Name";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 86);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(12, 70);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.Size = new System.Drawing.Size(42, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Gender";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(213, 86);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(160, 70);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(33, 17);
+            this.label4.Size = new System.Drawing.Size(26, 13);
             this.label4.TabIndex = 3;
             this.label4.Text = "Age";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(16, 11);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(12, 9);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(19, 17);
+            this.label5.Size = new System.Drawing.Size(15, 13);
             this.label5.TabIndex = 4;
             this.label5.Text = "id";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // id
             // 
-            this.id.Location = new System.Drawing.Point(103, 11);
-            this.id.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.id.Location = new System.Drawing.Point(77, 9);
             this.id.Name = "id";
-            this.id.Size = new System.Drawing.Size(88, 22);
+            this.id.Size = new System.Drawing.Size(214, 20);
             this.id.TabIndex = 5;
             this.id.TextChanged += new System.EventHandler(this.id_TextChanged);
             // 
             // fname
             // 
-            this.fname.Location = new System.Drawing.Point(103, 52);
-            this.fname.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.fname.Location = new System.Drawing.Point(77, 42);
             this.fname.Name = "fname";
-            this.fname.Size = new System.Drawing.Size(88, 22);
+            this.fname.Size = new System.Drawing.Size(67, 20);
             this.fname.TabIndex = 6;
             // 
             // lname
             // 
-            this.lname.Location = new System.Drawing.Point(299, 52);
-            this.lname.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lname.Location = new System.Drawing.Point(224, 42);
             this.lname.Name = "lname";
-            this.lname.Size = new System.Drawing.Size(88, 22);
+            this.lname.Size = new System.Drawing.Size(67, 20);
             this.lname.TabIndex = 7;
             // 
             // gender
@@ -136,81 +128,72 @@ namespace Employee_CRUD
             "Male",
             "Female",
             "Other"});
-            this.gender.Location = new System.Drawing.Point(103, 86);
-            this.gender.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gender.Location = new System.Drawing.Point(77, 70);
             this.gender.Name = "gender";
-            this.gender.Size = new System.Drawing.Size(88, 24);
+            this.gender.Size = new System.Drawing.Size(67, 21);
             this.gender.TabIndex = 8;
             // 
             // age
             // 
-            this.age.Location = new System.Drawing.Point(299, 87);
-            this.age.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.age.Location = new System.Drawing.Point(224, 71);
             this.age.Name = "age";
-            this.age.Size = new System.Drawing.Size(88, 22);
+            this.age.Size = new System.Drawing.Size(67, 20);
             this.age.TabIndex = 9;
             // 
             // cellphone
             // 
-            this.cellphone.Location = new System.Drawing.Point(101, 126);
-            this.cellphone.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cellphone.Location = new System.Drawing.Point(76, 102);
             this.cellphone.Name = "cellphone";
-            this.cellphone.Size = new System.Drawing.Size(88, 22);
+            this.cellphone.Size = new System.Drawing.Size(67, 20);
             this.cellphone.TabIndex = 11;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(16, 124);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(12, 101);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(76, 17);
+            this.label6.Size = new System.Drawing.Size(58, 13);
             this.label6.TabIndex = 10;
             this.label6.Text = "Cell Phone";
             // 
             // nationality
             // 
-            this.nationality.Location = new System.Drawing.Point(299, 126);
-            this.nationality.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nationality.Location = new System.Drawing.Point(224, 102);
             this.nationality.Name = "nationality";
-            this.nationality.Size = new System.Drawing.Size(88, 22);
+            this.nationality.Size = new System.Drawing.Size(67, 20);
             this.nationality.TabIndex = 13;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(213, 124);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Location = new System.Drawing.Point(160, 101);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(74, 17);
+            this.label7.Size = new System.Drawing.Size(56, 13);
             this.label7.TabIndex = 12;
             this.label7.Text = "Nationality";
             // 
             // dob
             // 
-            this.dob.Location = new System.Drawing.Point(103, 170);
-            this.dob.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dob.Location = new System.Drawing.Point(77, 138);
             this.dob.Name = "dob";
-            this.dob.Size = new System.Drawing.Size(265, 22);
+            this.dob.Size = new System.Drawing.Size(200, 20);
             this.dob.TabIndex = 14;
             this.dob.ValueChanged += new System.EventHandler(this.dob_ValueChanged);
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(15, 170);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Location = new System.Drawing.Point(11, 138);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(87, 17);
+            this.label8.Size = new System.Drawing.Size(66, 13);
             this.label8.TabIndex = 15;
             this.label8.Text = "Date of Birth";
             // 
             // insert
             // 
-            this.insert.Location = new System.Drawing.Point(20, 238);
-            this.insert.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.insert.Location = new System.Drawing.Point(15, 193);
             this.insert.Name = "insert";
-            this.insert.Size = new System.Drawing.Size(100, 28);
+            this.insert.Size = new System.Drawing.Size(75, 23);
             this.insert.TabIndex = 16;
             this.insert.Text = "Insert";
             this.insert.UseVisualStyleBackColor = true;
@@ -218,10 +201,9 @@ namespace Employee_CRUD
             // 
             // update
             // 
-            this.update.Location = new System.Drawing.Point(148, 238);
-            this.update.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.update.Location = new System.Drawing.Point(111, 193);
             this.update.Name = "update";
-            this.update.Size = new System.Drawing.Size(100, 28);
+            this.update.Size = new System.Drawing.Size(75, 23);
             this.update.TabIndex = 17;
             this.update.Text = "Update";
             this.update.UseVisualStyleBackColor = true;
@@ -229,10 +211,9 @@ namespace Employee_CRUD
             // 
             // delete
             // 
-            this.delete.Location = new System.Drawing.Point(299, 238);
-            this.delete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.delete.Location = new System.Drawing.Point(224, 193);
             this.delete.Name = "delete";
-            this.delete.Size = new System.Drawing.Size(100, 28);
+            this.delete.Size = new System.Drawing.Size(75, 23);
             this.delete.TabIndex = 18;
             this.delete.Text = "Delete";
             this.delete.UseVisualStyleBackColor = true;
@@ -240,10 +221,9 @@ namespace Employee_CRUD
             // 
             // read
             // 
-            this.read.Location = new System.Drawing.Point(443, 238);
-            this.read.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.read.Location = new System.Drawing.Point(332, 193);
             this.read.Name = "read";
-            this.read.Size = new System.Drawing.Size(100, 28);
+            this.read.Size = new System.Drawing.Size(75, 23);
             this.read.TabIndex = 19;
             this.read.Text = "Read";
             this.read.UseVisualStyleBackColor = true;
@@ -252,18 +232,20 @@ namespace Employee_CRUD
             // datagridview
             // 
             this.datagridview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.datagridview.Location = new System.Drawing.Point(20, 297);
+            this.datagridview.Location = new System.Drawing.Point(15, 241);
+            this.datagridview.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.datagridview.Name = "datagridview";
             this.datagridview.RowHeadersWidth = 51;
             this.datagridview.RowTemplate.Height = 24;
-            this.datagridview.Size = new System.Drawing.Size(523, 150);
+            this.datagridview.Size = new System.Drawing.Size(392, 122);
             this.datagridview.TabIndex = 20;
+            this.datagridview.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.datagridview_CellContentClick);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(421, 382);
             this.Controls.Add(this.datagridview);
             this.Controls.Add(this.read);
             this.Controls.Add(this.delete);
@@ -285,7 +267,6 @@ namespace Employee_CRUD
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
